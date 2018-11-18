cbc1f6b7dfa2b9b10107a9cc842cdde26513f465
github username <github email address>
2018-11-17 20:29:31 -0500

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,14 @@
+require "pry"
+
 def alphabetize(arr)
   # code here
+  esp_alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz" # this has index elements, almost as if this string is a list
+
+  arr.sort_by do |string| # break up array into strings
+    string.split("").map do |chr| #iterate on each character of string
+      esp_alpha.index(chr) # sort_by the position of character on the esp_alphabet
+      #binding.pry
+    end
+  end
+
 end
